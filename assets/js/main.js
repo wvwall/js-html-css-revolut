e367555e8022c055f29f79cecb7d6d86d4c6ef90
wvwall <waltervelardo@gmail.com>
2021-03-12 17:11:58 +0100

    1° dropdown jquery

---
+++ assets/js/main.js
@@ -0,0 +1,17 @@
+$(document).ready(function (){
+
+$("#features").mouseenter(function () { 
+    $(".dropdown_features").addClass("active");
+});
+$(".dropdown_features").mouseleave(function () { 
+    $(".dropdown_features").removeClass("active");
+});
+
+
+
+
+
+
+
+
+});
